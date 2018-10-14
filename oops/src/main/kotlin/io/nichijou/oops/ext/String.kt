eb8c05288880e875df21ba7e4cc56920951ec96f
iota9star <iota9star@gmail.com>
2018-10-14 15:02:44 +0800

    .........................

--- oops/src/main/kotlin/io/nichijou/oops/ext/String.kt
+++ oops/src/main/kotlin/io/nichijou/oops/ext/String.kt
@@ -1,6 +1,6 @@
 package io.nichijou.oops.ext
 
-// eg: ?android:attr/textColorPrimary to oops/OOPS_SIGNED__QM_android_COLON_attr_SLASH_textColorPrimary
+// eg: ?android:attr/textColorPrimary to OOPS_SIGNED__QM_android_COLON_attr_SLASH_textColorPrimary
 internal fun String.oopsSignedAttrName(): String {
     return "OOPS_SIGNED_" + this.replace("?", "_QM_")
             .replace("/", "_SLASH_")
