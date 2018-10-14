2acb152e9d2dac892daffc078ac2d10c6ca62097
iota9star <iota9star@gmail.com>
2018-10-14 14:46:34 +0800

    update ripple animation

--- oops/src/main/kotlin/io/nichijou/oops/RippleAnimation.kt
+++ oops/src/main/kotlin/io/nichijou/oops/RippleAnimation.kt
@@ -1,9 +1,5 @@
 package io.nichijou.oops
 
-/**
- * Created by wuyr on 3/15/18 5:23 PM.
- */
-
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
@@ -17,28 +13,22 @@ import android.view.ViewGroup
 import io.nichijou.oops.ext.activity
 import io.nichijou.oops.ext.getAbsoluteX
 import io.nichijou.oops.ext.getAbsoluteY
-import io.nichijou.oops.ext.loge
-
-/**
- * Created by wuyr on 3/15/18 5:23 PM.
- * GitHub: https://github.com/wuyr/RippleAnimation
- */
 
 @SuppressLint("ViewConstructor")
 class RippleAnimation private constructor(
         context: Context,
         private val mStartX: Float,
-        private val mStartY: Float,//扩散的起点
+        private val mStartY: Float,
         private val mStartRadius: Int
 ) : View(context) {
 
-    private var mBackground: Bitmap? = null//屏幕截图
-    private var mPaint: Paint? = null
+    private val mPaint: Paint = Paint()
+    private val mRootView: ViewGroup = context.activity().window.decorView as ViewGroup
+    private var mBackground: Bitmap? = null
     private var mMaxRadius: Float = 0f
     private var mCurrentRadius: Int = 0
     private var isStarted: Boolean = false
     private var mDuration: Long = 0
-    private var mRootView: ViewGroup? = null//DecorView
     private var mOnAnimationEndListener: OnAnimationEndListener? = null
     private var mAnimatorListener: Animator.AnimatorListener? = null
     private var mAnimatorUpdateListener: ValueAnimator.AnimatorUpdateListener? = null
@@ -52,24 +42,17 @@ class RippleAnimation private constructor(
         }
 
     init {
-        //获取activity的根视图,用来添加本View
-        mRootView = context.activity().window.decorView as ViewGroup
-        mPaint = Paint()
-        mPaint!!.isAntiAlias = true
-        //设置为擦除模式
-        mPaint!!.xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
-        updateMaxRadius()
+        mPaint.isAntiAlias = true
+        mPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.CLEAR)
+        updateRadius()
         initListener()
     }
 
-    /**
-     * 开始播放动画
-     */
     fun start() {
         if (!isStarted) {
             isStarted = true
             updateBackground()
-            attachToRootView()
+            startAndAttach()
             animator.start()
         }
     }
@@ -82,9 +65,6 @@ class RippleAnimation private constructor(
         }
     }
 
-    /**
-     * 设置动画时长
-     */
     fun setDuration(duration: Long): RippleAnimation {
         mDuration = duration
         return this
@@ -93,103 +73,74 @@ class RippleAnimation private constructor(
     private fun initListener() {
         mAnimatorListener = object : AnimatorListenerAdapter() {
             override fun onAnimationEnd(animation: Animator) {
+                endAndDetach()
                 isStarted = false
-                //动画播放完毕, 移除本View
-                detachFromRootView()
-                if (mOnAnimationEndListener != null) {
-                    mOnAnimationEndListener!!.onAnimationEnd()
-                }
             }
         }
         mAnimatorUpdateListener = ValueAnimator.AnimatorUpdateListener { animation ->
-            //更新圆的半径
             mCurrentRadius = (animation.animatedValue as Float).toInt() + mStartRadius
             postInvalidate()
         }
     }
 
-    /**
-     * 根据起始点将屏幕分成4个小矩形,mMaxRadius就是取它们中最大的矩形的对角线长度
-     * 这样的话, 无论起始点在屏幕中的哪一个位置上, 我们绘制的圆形总是能覆盖屏幕
-     */
-    private fun updateMaxRadius() {
-        //将屏幕分成4个小矩形
+    private fun updateRadius() {
         val leftTop = RectF(0f, 0f, mStartX + mStartRadius, mStartY + mStartRadius)
-        val rightTop = RectF(leftTop.right, 0f, mRootView!!.right.toFloat(), leftTop.bottom)
-        val leftBottom = RectF(0f, leftTop.bottom, leftTop.right, mRootView!!.bottom.toFloat())
-        val rightBottom = RectF(leftBottom.right, leftTop.bottom, mRootView!!.right.toFloat(), leftBottom.bottom)
-        //分别获取对角线长度
+        val rightTop = RectF(leftTop.right, 0f, mRootView.right.toFloat(), leftTop.bottom)
+        val leftBottom = RectF(0f, leftTop.bottom, leftTop.right, mRootView.bottom.toFloat())
+        val rightBottom = RectF(leftBottom.right, leftTop.bottom, mRootView.right.toFloat(), leftBottom.bottom)
         val leftTopHypotenuse = Math.sqrt(Math.pow(leftTop.width().toDouble(), 2.0) + Math.pow(leftTop.height().toDouble(), 2.0))
         val rightTopHypotenuse = Math.sqrt(Math.pow(rightTop.width().toDouble(), 2.0) + Math.pow(rightTop.height().toDouble(), 2.0))
         val leftBottomHypotenuse = Math.sqrt(Math.pow(leftBottom.width().toDouble(), 2.0) + Math.pow(leftBottom.height().toDouble(), 2.0))
         val rightBottomHypotenuse = Math.sqrt(Math.pow(rightBottom.width().toDouble(), 2.0) + Math.pow(rightBottom.height().toDouble(), 2.0))
-        //取最大值
         mMaxRadius = Math.max(Math.max(leftTopHypotenuse, rightTopHypotenuse), Math.max(leftBottomHypotenuse, rightBottomHypotenuse)).toFloat()
     }
 
-    /**
-     * 添加到根视图
-     */
-    private fun attachToRootView() {
-        layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
-        mRootView!!.addView(this)
+
+    private fun startAndAttach() {
+        this.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        mRootView.addView(this)
     }
 
-    /**
-     * 从根视图中移除并释放资源
-     */
-    private fun detachFromRootView() {
-        if (mRootView != null) {
-            mRootView!!.removeView(this)
-            mRootView = null
-        }
-        if (mBackground != null) {
-            if (!mBackground!!.isRecycled) {
-                mBackground!!.recycle()
-            }
-            mBackground = null
-        }
-        if (mPaint != null) {
-            mPaint = null
+
+    private fun endAndDetach() {
+        mOnAnimationEndListener?.onAnimationEnd()
+        mRootView.removeView(this)
+        mBackground = mBackground?.let {
+            if (!it.isRecycled) it.recycle()
+            null
         }
         mAnimatorListener = null
         mAnimatorUpdateListener = null
         mOnAnimationEndListener = null
     }
 
-    /**
-     * 更新屏幕截图
-     */
+
     private fun updateBackground() {
-        if (mBackground != null && !mBackground!!.isRecycled) {
-            mBackground!!.recycle()
+        mBackground?.let {
+            if (!it.isRecycled) {
+                it.recycle()
+            }
         }
-        mRootView!!.isDrawingCacheEnabled = true
-        mBackground = mRootView!!.drawingCache
-        if (mBackground != null) {
-            mBackground = Bitmap.createBitmap(mBackground!!)
-        } else {
-            loge(IllegalStateException()) { "ripple background is null..." }
+        mRootView.isDrawingCacheEnabled = true
+        mBackground = mRootView.drawingCache
+        mBackground = mBackground?.let {
+            Bitmap.createBitmap(it)
         }
-        mRootView!!.isDrawingCacheEnabled = false
+        mRootView.isDrawingCacheEnabled = false
     }
 
     override fun onDraw(canvas: Canvas) {
         if (mBackground == null) return
-        //在新的图层上面绘制
-        val layer: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        val layer = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             canvas.saveLayer(0f, 0f, width.toFloat(), height.toFloat(), null)
         } else {
             canvas.saveLayer(0f, 0f, width.toFloat(), height.toFloat(), null, Canvas.ALL_SAVE_FLAG)
         }
         canvas.drawBitmap(mBackground!!, 0f, 0f, null)
-        canvas.drawCircle(mStartX, mStartY, mCurrentRadius.toFloat(), mPaint!!)
+        canvas.drawCircle(mStartX, mStartY, mCurrentRadius.toFloat(), mPaint)
         canvas.restoreToCount(layer)
     }
 
-    /**
-     * 设置动画结束监听器
-     */
     fun setOnAnimationEndListener(listener: OnAnimationEndListener): RippleAnimation {
         mOnAnimationEndListener = listener
         return this
@@ -209,11 +160,8 @@ class RippleAnimation private constructor(
             val context = onClickView.context
             val newWidth = onClickView.width / 2
             val newHeight = onClickView.height / 2
-            //计算起点位置
             val startX = onClickView.getAbsoluteX() + newWidth
             val startY = onClickView.getAbsoluteY() + newHeight
-            //起始半径
-            //因为我们要避免遮挡按钮
             val radius = Math.max(newWidth, newHeight)
             return RippleAnimation(context, startX, startY, radius)
         }
